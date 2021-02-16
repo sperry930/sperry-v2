54895a7c81eff487312061e8b9e41a04db6e0d84
yaniv goldobin <ygoldobin@gmail.com>
2021-02-15 19:30:49 -0800

    padding magic

--- CONTENT_MAP.js
+++ CONTENT_MAP.js
@@ -15,14 +15,14 @@ export default {
             width: "50%",
           },
           {
-             type: "image",
+            type: "image",
             name: "nike/nike-6.webp",
             width: "50%",
           },
           {
             type: "video",
             name: "beats/intro.mp4",
-            width: "100%",   
+            width: "100%",
           },
           {
             type: "image",
@@ -93,6 +93,21 @@ export default {
             type: "image",
             name: "drama/drama-12.webp",
             width: "50%",
+          },
+          {
+            type: "embed",
+            name: "https://player.vimeo.com/video/329862491",
+            width: "50%",
+          },
+          {
+            type: "image",
+            name: "drama/drama-12.webp",
+            width: "50%",
+          },
+          {
+            type: "embed",
+            name: "https://player.vimeo.com/video/333018949",
+            width: "100%",
           },
         ],
       },
@@ -263,3 +278,11 @@ export default {
     ],
   },
 }
+
+/* 
+<div style="padding:61.36% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/329862491" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+<p><a href="https://vimeo.com/329862491">Omar Apollo | Years To Come (dir. Scott Perry)</a> from <a href="https://vimeo.com/patrickgolan">Patrick Golan</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+
+<div style="padding:41.89% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/333018949" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+<p><a href="https://vimeo.com/333018949">Homegrown</a> from <a href="https://vimeo.com/sperrystuff">sperry</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+*/
--- src/components/content-page.js
+++ src/components/content-page.js
@@ -16,8 +16,109 @@ const getContentMarginTop = () => {
   return "150px"
 }
 
+const MyVideo = ({ video }) => {
+  return (
+    <video
+      key={video.name}
+      name={video.name}
+      alt={video.name}
+      style={{
+        width: `calc(${video.width} - 10px)`,
+        margin: "0 5px",
+      }}
+      src={"../" + video.name}
+      preload="auto"
+      autoPlay
+      playsInline
+      muted
+      loop
+    ></video>
+  )
+}
+
+const MyImage = ({ image }) => {
+  return (
+    <img
+      key={image.name}
+      alt={image.name}
+      style={{
+        width: `calc(${image.width} - 10px)`,
+        margin: "0 5px",
+      }}
+      src={"../" + image.name}
+    ></img>
+  )
+}
+
+/* 
+<div style="padding:61.36% 0 0 0;position:relative;"><iframe src="https://player.vimeo.com/video/329862491" style="position:absolute;top:0;left:0;width:100%;height:100%;" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe></div><script src="https://player.vimeo.com/api/player.js"></script>
+<p><a href="https://vimeo.com/329862491">Omar Apollo | Years To Come (dir. Scott Perry)</a> from <a href="https://vimeo.com/patrickgolan">Patrick Golan</a> on <a href="https://vimeo.com">Vimeo</a>.</p>
+*/
+const MyEmbed = ({ embed }) => {
+  if (embed.width === "100%") {
+    return (
+      <div
+        style={{
+          padding: "61.36% 0 0 0",
+          position: "relative",
+          width: `calc(${embed.width} - 10px)`,
+          margin: "0 5px",
+          display: "inline-block",
+          background: "black",
+        }}
+      >
+        <iframe
+          key={embed.name}
+          title={embed.name}
+          src={embed.name}
+          frameBorder="0"
+          allow="autoplay; fullscreen; picture-in-picture"
+          allowFullScreen
+          style={{
+            marginBottom: 0,
+            position: "absolute",
+            top: 0,
+            left: 0,
+            width: "100%",
+            height: "100%",
+          }}
+        ></iframe>
+      </div>
+    )
+  }
+
+  return (
+    <div
+      style={{
+        padding: "36.68% 0 0 0",
+        position: "relative",
+        width: `calc(${embed.width} - 10px)`,
+        margin: "0 5px",
+        display: "inline-block",
+        background: "black",
+      }}
+    >
+      <iframe
+        key={embed.name}
+        src={embed.name}
+        title={embed.name}
+        frameBorder="0"
+        allow="autoplay; fullscreen; picture-in-picture"
+        allowFullScreen
+        style={{
+          marginBottom: 0,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          height: "100%",
+        }}
+      ></iframe>
+    </div>
+  )
+}
+
 const ContentPage = ({ data }) => {
-  console.warn(`getContentMarginTop()`, getContentMarginTop())
   return (
     <Layout>
       <div
@@ -32,36 +133,16 @@ const ContentPage = ({ data }) => {
             return (
               <div key={index} style={{ margin: "50px 0px" }}>
                 <div style={{ margin: "50px 5px 20px" }}>{project.title}</div>
-                {project.content.map(imageOrVideo => {
-                  if (imageOrVideo.type === "video") {
-                    return (
-                      <video
-                        key={imageOrVideo.name}
-                        alt={imageOrVideo.name}
-                        style={{
-                          width: `calc(${imageOrVideo.width} - 10px)`,
-                          margin: "0 5px",
-                        }}
-                        src={"../" + imageOrVideo.name}
-                        preload="auto"
-                        autoPlay
-                        playsInline
-                        muted
-                        loop
-                      ></video>
-                    )
+                {project.content.map((imageOrVideo, index) => {
+                  if (imageOrVideo.type === "embed") {
+                    return <MyEmbed key={index} embed={imageOrVideo} />
                   }
-                  return (
-                    <img
-                      key={imageOrVideo.name}
-                      alt={imageOrVideo.name}
-                      style={{
-                        width: `calc(${imageOrVideo.width} - 10px)`,
-                        margin: "0 5px",
-                      }}
-                      src={"../" + imageOrVideo.name}
-                    ></img>
-                  )
+
+                  if (imageOrVideo.type === "video") {
+                    return <MyVideo key={index} video={imageOrVideo} />
+                  }
+
+                  return <MyImage key={index} image={imageOrVideo} />
                 })}
               </div>
             )
@@ -73,8 +154,3 @@ const ContentPage = ({ data }) => {
 }
 
 export default ContentPage
-
-/*
-                      <iframe src="https://player.vimeo.com/video/329862491" width="640" height="393" frameborder="0" allow="autoplay; fullscreen; picture-in-picture" allowfullscreen></iframe>
-
-*/
